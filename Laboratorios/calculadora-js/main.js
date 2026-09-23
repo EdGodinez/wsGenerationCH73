abfd8915e356951b37610ccb53065f3434eeb929
Eduardo <eduardo_gdz@outlook.com>
2026-09-22 18:07:19 -0600

    Agrega laboratorio calculadora-js

---
+++ Laboratorios/calculadora-js/main.js
@@ -0,0 +1,14 @@
+let nombre = prompt("¿Como te llamas?");
+let numero_1 = Number(prompt("Escribe el primer numero"));
+let numero_2 = Number(prompt("Escribe el segundo numero"));
+
+let suma = numero_1 + numero_2;
+let resta = numero_1 - numero_2;
+let multiplicacion = numero_1 * numero_2;
+let division = numero_1 / numero_2
+
+console.log("Hola " + nombre + ", estos son tus resultados: ");
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicacion: " + multiplicacion);
+console.log("Division: " + division);
